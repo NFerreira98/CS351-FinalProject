bc3648d59c5b90fef0739c4182a1b1d306b75c1c
tecknickle <70725839+tecknickle@users.noreply.github.com>
2024-04-15 23:22:29 -0700

    add files/test

---
+++ controllers/database.js
@@ -0,0 +1,19 @@
+//import uri from './databaseConnection.js';
+//the databaseConnection.js is inside the same controllers folder and ONLY has one line the connectivity uri
+// to connect your NodeJS code to YOUR MongoDB instance
+// module.exports = { uri: 'mongodb+srv://YOUR_Login:YOUR_Password@cluster0.WHATEVER.mongodb.net/?retryWrites=true&w=majority' };
+
+
+var { uri } = require('./databaseConnection');
+
+//Define some varibles needed for the database Controller functions
+const { MongoClient, ServerApiVersion, FindCursor} = require('mongodb');
+
+// SETP 1: Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
