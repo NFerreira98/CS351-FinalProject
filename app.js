cfa20b266e70550a8f3b06a3d23510900e0a1f89
helguera94 <helguera.jen@gmail.com>
2024-04-24 20:17:12 -0700

    made createAccount work using html file and saving to database

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users');
 var cartRouter = require('./routes/cart');
 var storeAcountRouter = require('./routes/createAcount')
 
@@ -23,9 +23,9 @@ app.use(express.static(path.join(__dirname,'Project1')));
 
 
 // app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/users', usersRouter);
 app.use('/shoppingCart',cartRouter);
-app.use('/storeeAccount',storeAcountRouter)
+app.use('/storeAccount',storeAcountRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
