34a2172d3ba6a51196304cec5dc4e787aa796800
Jenn <59626610+helguera94@users.noreply.github.com>
2024-04-26 20:12:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var checkOutRouter = require('./routes/checkOut');
 var addToCartRouter = require('./routes/addToCart');
+var cartRouter = require('./routes/cart');
 
 var app = express();
 
@@ -49,7 +50,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/checkOut', checkOutRouter);
 app.use('/addToCart', addToCartRouter);
-
+app.use('/shoppingCart',cartRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
