126046d35b1ebc2491199f9a3e52f1143792e96a
NFerreira98 <142639216+NFerreira98@users.noreply.github.com>
2024-04-28 01:59:29 -0700

    Comments added

--- routes/createAcount.js
+++ routes/createAcount.js
@@ -1,26 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-//########################################
+//Necessary for server-side validation
+const { body, validationResult } = require('express-validator');
+
+
 //to process data sent in on request need body-parser module
 var bodyParser = require('body-parser');
-var path = require ('path'); //to work with separtors on any OS including Windows
+var path = require('path'); //to work with separtors on any OS including Windows
 var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
 
-router.use(bodyParser.json()); // for parsing application/json
-
+router.use(bodyParser.json()); // for parsing application/jsona
 router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-//#########################################
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    // res.sendFile('createAccount.html',{root:'./Project1'})
-   res.redirect('https://csweb01.csueastbay.edu/~bo5237/Project1/createAccount.html')
-    // res.render('storeAccount', { title: 'ReadFormDataSaveMongoDB' });
+
+// Server-side validation
+const validateUsername = body('username').isLength({ min: 4 }).withMessage('Username must be at least 4 characters long');
+const validateEmail = body('email').isEmail().withMessage('Invalid email address');
+const validatePassword = body('password').isLength({ min: 7 }).withMessage('Password must be at least 7 characters long')
+    .matches(/\d/).withMessage('Password must contain at least one number');
+const validateConfirmPassword = body('confirm-password').custom((value, { req }) => {
+    if (value !== req.body.password) {
+        throw new Error('Passwords do not match');
+    }
+    return true;
 });
 
+/* GET createAccount page. */
+router.get('/', function(req, res, next) {
+    res.redirect('https://csweb01.csueastbay.edu/~bo5237/Project1/createAccount.html');
+});
 
-var controllerDatabase = require('../controllers/database');   //this will load the controller file below
-router.post("/", controllerDatabase.saveNewCustomer); //see controllers/database.js file
+var controllerDatabase = require('../controllers/database');
+router.post("/", [
+    validateUsername,
+    validateEmail,
+    validatePassword,
+    validateConfirmPassword
+], controllerDatabase.saveNewCustomer);
 
 module.exports = router;
