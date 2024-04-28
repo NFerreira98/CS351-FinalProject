39ce20d68ee15faed642f780cf3ef0b88a02f692
NFerreira98 <142639216+NFerreira98@users.noreply.github.com>
2024-04-28 02:05:57 -0700

    Update loginSuccess.js

--- routes/loginSuccess.js
+++ routes/loginSuccess.js
@@ -1,10 +1,26 @@
 var express = require('express');
-const {getAccount} = require("../controllers/database");
+const { checkLogin } = require("../controllers/database");
 var router = express.Router();
 
-router.get('/',getAccount)
+// Handle POST request for user login
+router.post('/', async function(req, res, next) {
+    const { username, password } = req.body;
 
+    try {
+        // Check user login
+        const user = await checkLogin(username, password);
 
-
+        if (user) {
+            // If login is successful, render the login success view
+            res.render('loginSuccess', { title: 'Login Success', user });
+        } else {
+            // If login fails, go back to account login page
+            res.redirect("https://csweb01.csueastbay.edu/~bo5237/Project1/account.html")        }
+    } catch (error) {
+        console.error('Error logging in:', error);
+        // Render an error page if something goes wrong
+        res.render('error', { title: 'Error', errorMessage: 'An error occurred while logging in.' });
+    }
+});
 
 module.exports = router;
